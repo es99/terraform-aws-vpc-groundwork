12235ccceb7c89f97b27ad7885447865430e97d6
engels <engels.franca@icloud.com>
2024-11-06 23:42:48 -0300

    fix: 1.1.1 -> 1.1.2

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,9 +1,9 @@
 module "vpc-groundwork" {
   source  = "es99/vpc-groundwork/aws"
-  version = "1.1.1"
+  version = "1.1.2"
 
-  role = var.role
+  role             = var.role
   database_subnets = var.database_subnets
-  private_subnets =  var.private_subnets
-  public_subnets =  var.public_subnets
+  private_subnets  = var.private_subnets
+  public_subnets   = var.public_subnets
 }
